3779692bdfde6501e4cf36420cbbca552fcf9aa1
Yang Ke <saint_yk@163.com>
2017-07-10 17:15:39 +0800

    spread operator on arrays

--- app/js/index.js
+++ app/js/index.js
@@ -7,11 +7,26 @@ import {
 	logTitle
 } from 'logger';
 /* your imports */
-logTitle('Template Literals');
+logTitle('Spread Operator Arrays');
 /* coding examples */
-const name = "Anna";
-const country = "Canada";
-const age = 33;
+const arrayOne = ['Marian', 'Anna', 'Alex'];
+const arrayTwo = ['Sald', 'Ismall', 'Aisha'];
 
-log("Name: " + name + " Country: " + country + " Age: " + age);
-log(`Name: ${name} Country: ${country} Age: ${age}`);
+// array
+const concatArray = [...arrayOne, ...arrayTwo];
+
+concatArray.forEach((name) => {
+	log(name);
+});
+
+// string
+const name = 'Marian';
+const nameToArray = [...name];
+nameToArray.forEach((letter) => {
+	log(letter);
+});
+
+// functions
+const addNumbers = (n1, n2, n3) => n1 + n2 + n3;
+const numbers = [1, 5, 9];
+log(addNumbers(...numbers));
