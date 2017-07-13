d2312d717b14364ea73bb6b687ee41471b059d94
Yang Ke <saint_yk@163.com>
2017-07-13 10:36:48 +0800

    Generators Basics

--- app/js/index.js
+++ app/js/index.js
@@ -7,70 +7,49 @@ import {
 	logTitle
 } from 'logger';
 /* your imports */
-logTitle('Promise Object');
+logTitle('Generators');
 /* coding examples */
 
-/*const promise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('data back from the server');
-	}, 3000);
-
-	// this method will be invoked first
-	setTimeout(() => {
-		reject('no data back from the server, there was an error')
-	}, 2000);
-});
-
-promise.then(response => {
-	log(response);
-}).catch(error => {
-	log(error);
-});*/
-
-// promise all
-
-const namesPromise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve(['Anna', 'Jones', 'Ali', 'Jake']);
-	}, 3000);
-
-	setTimeout(() => {
-		reject('no data back from server, there was an error');
-	}, 4000);
-});
-
-const surnamesPromise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve(['Williams', 'Bravo', 'Mohammado', 'Smith']);
-	}, 3000);
-});
-
-/*Promise.all([namesPromise, surnamesPromise])
-	.then(data => {
-		//log(data);
-		const [names, surnames] = data;
-		for (var i=0; i<names.length; i++) {
-			const name = names[i];
-			const surname = surnames[i];
-			log(`${name} ${surname}`);
-		}
-	})
-	.catch(error => {
-		log(error);
-	});*/
-
-	// realworld promise
-
-const getRandomUsers = n => {
-	const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`);
-	fetchRandomUsers.then(data => {
-		data.json().then(randomUsers => {
-			randomUsers.results.forEach(user => {
-				const {gender, email} = user;
-				log(`${gender} ${email}`);
-			});
-		});
-	});
+/* Generators Basic */
+const getNumbers = function* () {
+	yield 1;
+	yield 'hello';
+	yield true;
+	yield { name: 'Alex', }
+	return 'I am done';
 }
 
-getRandomUsers(50);
+const numbersGen = getNumbers();
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+
+/* Generators More */
+
+const enhancedGetNumbers = function* (numbers) {
+	for (var i=0; i<numbers.length; i++) {
+		yield numbers[i];
+	}
+}
+
+const enhancedNubmersGen = enhancedGetNumbers([1,2,3,4,5]);
+const interval = setInterval(() => {
+	const next = enhancedNubmersGen.next();
+	if(next.done) {
+		log('this generator is done');
+		clearInterval(interval);
+	} else {
+		const number = next.value;
+		log(number);
+	}
+}, 1000);
+
+/* Generators, Promises, Coroutines */
+
+
+
+
+
